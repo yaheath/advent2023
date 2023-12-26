cc70baca05f54181f7d3f1de92a259f677177023
Heath Kehoe <yaheath@gmail.com>
2023-12-26 12:00:56 -0600

    days 24, 25

---
+++ src/bin/day24.rs
@@ -0,0 +1,114 @@
+use std::ops::RangeInclusive;
+use std::str::FromStr;
+use std::vec::Vec;
+use itertools::Itertools;
+use ya_advent_lib::geom::{Point2D, Point3D, Ray2D};
+use ya_advent_lib::read::read_input;
+
+#[derive(Copy, Clone)]
+struct Stone {
+    pos: Point3D,
+    vel: Point3D,
+}
+
+impl FromStr for Stone {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (p, v) = s.split_once(" @ ").unwrap();
+        let pos = p.parse::<Point3D>().unwrap();
+        let vel = v.parse::<Point3D>().unwrap();
+        Ok(Stone{pos, vel})
+    }
+}
+
+impl Stone {
+    fn intersect_2d(&self, other: &Stone, range: &RangeInclusive<f64>) -> bool {
+        let a = Ray2D::new(Point2D::new(self.pos.x, self.pos.y), Point2D::new(self.vel.x, self.vel.y));
+        let b = Ray2D::new(Point2D::new(other.pos.x, other.pos.y), Point2D::new(other.vel.x, other.vel.y));
+        if let Some(c) = a.intersect_with(&b) {
+            range.contains(&c.x) && range.contains(&c.y)
+        }
+        else {
+            false
+        }
+    }
+}
+
+fn num_2d_intersections(input: &Vec<Stone>, range: RangeInclusive<f64>) -> usize {
+    input.iter()
+        .tuple_combinations()
+        .filter(|(a, b)| a.intersect_2d(b, &range))
+        .count()
+}
+
+fn part1(input: &Vec<Stone>) -> usize {
+    num_2d_intersections(input, 200000000000000.0..=400000000000000.0)
+}
+
+fn independent(a: Point3D, b: Point3D) -> bool {
+    let c = a.cross(b);
+    c.x != 0.0 || c.y != 0.0 || c.z != 0.0
+}
+
+fn find_plane(s1: &Stone, s2: &Stone) -> (Point3D, f64) {
+    let pdiff = s1.pos - s2.pos;
+    let vdiff = s1.vel - s2.vel;
+    let vv = s1.vel.cross(s2.vel);
+    (pdiff.cross(vdiff), pdiff.dot(vv))
+}
+
+fn lin(r: f64, a: Point3D, s: f64, b: Point3D, t: f64, c: Point3D) -> Point3D {
+    Point3D::new(
+         r * a.x + s * b.x + t * c.x,
+         r * a.y + s * b.y + t * c.y,
+         r * a.z + s * b.z + t * c.z,
+    )
+}
+
+fn part2(input: &Vec<Stone>) -> i64 {
+    /* this comes from:
+https://www.reddit.com/r/adventofcode/comments/18pnycy/comment/kersplf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    */
+    let (stone_a, stone_b, stone_c) = input.iter()
+        .tuple_combinations()
+        .find(|(a, b, c)| independent(a.vel, b.vel) && independent(a.vel, c.vel) && independent(b.vel, c.vel))
+        .unwrap();
+    let (a, aa) = find_plane(stone_a, stone_b);
+    let (b, bb) = find_plane(stone_a, stone_c);
+    let (c, cc) = find_plane(stone_b, stone_c);
+
+    let w = lin(aa, b.cross(c), bb, c.cross(a), cc, a.cross(b));
+    let t = a.dot(b.cross(c));
+
+    let w = Point3D::new((w.x / t).round(), (w.y / t).round(), (w.z / t).round());
+    let w1 = stone_a.vel - w;
+    let w2 = stone_b.vel - w;
+    let ww = w1.cross(w2);
+
+    let ee = ww.dot(stone_b.pos.cross(w2));
+    let ff = ww.dot(stone_a.pos.cross(w1));
+    let gg = stone_a.pos.dot(ww);
+    let ss = ww.dot(ww);
+
+    let rock = lin(ee, w1, -ff, w2, gg, ww);
+    ((rock.x + rock.y + rock.z) / ss) as i64
+}
+
+fn main() {
+    let input: Vec<Stone> = read_input();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ya_advent_lib::read::test_input;
+
+    #[test]
+    fn day24_test() {
+        let input: Vec<Stone> = test_input(include_str!("day24.testinput"));
+        assert_eq!(num_2d_intersections(&input, 7.0..=27.0), 2);
+        assert_eq!(part2(&input), 47);
+    }
+}
---
+++ src/bin/day25.rs
@@ -0,0 +1,156 @@
+use std::cmp::Reverse;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::fs::File;
+use std::io::prelude::*;
+use std::str::FromStr;
+use std::vec::Vec;
+use itertools::Itertools;
+use rand::seq::IteratorRandom;
+use ya_advent_lib::read::read_input;
+
+#[derive(Clone)]
+struct Input {
+    comp: String,
+    connections: HashSet<String>,
+}
+
+impl FromStr for Input {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (comp, l) = s.split_once(": ").unwrap();
+        let connections = HashSet::from_iter(l.split(' ').map(|ss| ss.into()));
+        Ok(Input{comp: comp.into(), connections})
+    }
+}
+
+#[allow(dead_code)]
+fn dot(input: &Vec<Input>) -> std::io::Result<()> {
+    let mut file = File::create("day25.dot")?;
+    write!(file, "graph G {{")?;
+    for i in input {
+        for j in &i.connections {
+            write!(file, " {} -- {};", i.comp, j)?;
+        }
+    }
+    write!(file, "}}")?;
+    file.sync_all()?;
+    Ok(())
+}
+
+#[derive(Clone)]
+struct Graph {
+    nodes: HashSet<String>,
+    edges: HashMap<String, HashSet<String>>,
+}
+
+impl Graph {
+    fn new() -> Self {
+        Self {
+            nodes: HashSet::new(),
+            edges: HashMap::new(),
+        }
+    }
+    fn insert_edge(&mut self, a: &String, b: &String) {
+        self.nodes.insert(a.clone());
+        self.nodes.insert(b.clone());
+        self.edges.entry(a.clone())
+            .and_modify(|e| {(*e).insert(b.clone());})
+            .or_insert(HashSet::from_iter([b.clone()]));
+        self.edges.entry(b.clone())
+            .and_modify(|e| {(*e).insert(a.clone());})
+            .or_insert(HashSet::from_iter([a.clone()]));
+    }
+
+    fn remove_edge(&mut self, a: &String, b: &String) {
+        self.edges.get_mut(a).unwrap().remove(b);
+        self.edges.get_mut(b).unwrap().remove(a);
+    }
+
+    fn count_nodes(&self, anchor: &String) -> usize {
+        let mut visited: HashSet<String> = HashSet::new();
+        let mut queue: Vec<&String> = Vec::new();
+        queue.push(anchor);
+        while let Some(node) = queue.pop() {
+            visited.insert(node.clone());
+            self.edges[node].iter().for_each(|n| {
+                if !visited.contains(n) {
+                    queue.push(n);
+                }
+            });
+        }
+        visited.len()
+    }
+
+    fn path_between(&self, a: &String, b: &String) -> Option<Vec<String>> {
+        let mut queue: VecDeque<(String, Vec<String>)> = VecDeque::new();
+        let mut traversed: HashSet<String> = HashSet::new();
+        queue.push_back((a.clone(), Vec::new()));
+        while let Some((node, path)) = queue.pop_front() {
+            traversed.insert(node.clone());
+            let mut path = path.clone();
+            path.push(node.clone());
+            if node == *b {
+                return Some(path);
+            }
+            self.edges[&node].iter()
+                .filter(|n| !traversed.contains(*n))
+                .for_each(|n| queue.push_back((n.clone(), path.clone())));
+        }
+        None
+    }
+}
+
+fn part1(input: &Vec<Input>) -> usize {
+    // let _ = dot(input);
+    let mut graph = Graph::new();
+    for i in input.iter() {
+        for j in &i.connections {
+            graph.insert_edge(&i.comp, j);
+        }
+    }
+    let mut seen_edges: HashMap<(String,String),usize> = HashMap::new();
+    loop {
+        for _ in 0..20 {
+            let nodes = graph.nodes.iter().choose_multiple(&mut rand::thread_rng(), 2);
+            if let Some(path) = graph.path_between(nodes[0], nodes[1]) {
+                path
+                .iter()
+                .tuple_windows()
+                .for_each(|(a, b)| {
+                    let key = (a.min(b).clone(), a.max(b).clone());
+                    seen_edges.entry(key).and_modify(|n| *n += 1).or_insert(1);
+                });
+            }
+        }
+        let rem = seen_edges.iter()
+            .sorted_by_key(|(_, n)| Reverse(*n))
+            .map(|(k, _)| k)
+            .take(3)
+            .collect::<Vec<_>>();
+
+        rem.iter().for_each(|(a, b)| graph.remove_edge(a, b));
+        let n1 = graph.count_nodes(&rem[0].0);
+        let n2 = graph.count_nodes(&rem[0].1);
+        if n1 + n2 == graph.nodes.len() {
+            return n1 * n2;
+        }
+        rem.iter().for_each(|(a, b)| graph.insert_edge(a, b));
+    }
+}
+
+fn main() {
+    let input: Vec<Input> = read_input();
+    println!("Part 1: {}", part1(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ya_advent_lib::read::test_input;
+
+    #[test]
+    fn day25_test() {
+        let input: Vec<Input> = test_input(include_str!("day25.testinput"));
+        assert_eq!(part1(&input), 54);
+    }
+}
